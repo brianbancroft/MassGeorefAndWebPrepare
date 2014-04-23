500ebea50c58a5d11ba73d56e279b0c9459b8b58
Brian Bancroft <bancroft.bw@gmail.com>
2014-04-23 16:53:07 -0400

    Housekeeping
    I've added commenting to make the code more readable.

--- Scripts/appendSuffix.py
+++ Scripts/appendSuffix.py
@@ -1,20 +1,26 @@
-#Prepare Folders
-import arcpy,os
+#This script appends the file names of all tifs in a directory 
+#to use the special nomenclature for the special georeference tool.
+import arcpy
+import os
 
+
+#function - find all tiffs in the specified directory
 def getTiffs(folder):
     tifList = []
     list = os.listdir(folder)
     for l in list:
-        if l[-4:] == ".tif":
+        if l[-4:] == ".tif" or l[-4:] == ".TIF":
            tifList.append(l)
     del list
     return tifList
 
+#function - Appends the characters of the file chosen
 def appendFiles(list,folder,char):
     for l in list:
         new = l[:-4] + char + ".tif"
         os.rename(folder + "\\" + l, folder + "\\" + new)
 
+#START
 cropFolder = arcpy.GetParameterAsText(0)
 mapFolder = arcpy.GetParameterAsText(1)
 
