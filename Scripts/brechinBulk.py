d47cc9103addecc8938b26e92f88bac4c3f30b64
Brian Bancroft <bancroft.bw@gmail.com>
2014-04-23 16:29:42 -0400

    Housekeeping and Future Scope
    Cleaned commenting and added some prototyping so that this program seeks jpeg files as opposed to tiffs. 
    
    Removed an extra call to remove duplicate features.

--- Scripts/brechinBulk.py
+++ Scripts/brechinBulk.py
@@ -4,10 +4,14 @@
 #This is intended to be run in ArcGIS 10.1 or higher.  
 
 #import basic modules
-import os, arcpy, shutil
+import os
+import arcpy
+import shutil
 
 #import all modules from 3rd parties
-import xlrd, xlwt, zipfile
+import xlrd
+import xlwt
+import zipfile
 
 #import local modules
 import secondary_functions
@@ -15,48 +19,43 @@ import error_handling
 import tiling
 import georef
 
-#Get directory with Excel Files (Parameter - from arcgis tool)
 
 
 # Output directory:
-#outputDir = "C:/Temp/outputDir" 
 outputDir = arcpy.GetParameterAsText(0)
 
 # Excel Files, multipart input:
-#fileDir = "C:/Temp/" 
 workbookName = arcpy.GetParameterAsText(1)
 
 #Generic Metadata file.  Custom metadata out of scope for this project. Optional
 metadataLoc = arcpy.GetParameterAsText(2)
 
-#3. geotif directory:
-#tifDir = "C:/Temp/tiffs"
+#Geotif Directory
 tifDir = arcpy.GetParameterAsText(3)
 
-#4. Special Tiff directory. Optional
+#Special Tiff directory. Optional
 whiteDir = arcpy.GetParameterAsText(4)
 
-#5 Spatial Coordinate System
+#Spatial Coordinate System 
 coordSys = arcpy.GetParameter(5)
 coordSys = coordSys.factoryCode
 
-#6. union shape file (Optional)
+#Union Shape File - Also an output
 unionShapeFileZip = arcpy.GetParameterAsText(6)
 unionShapefile = ""
 
-#######################END PARAMETERS#####################################
-georefToggle = False
+#FUTURE SCOPE: Jpeg bool - uses JPEGS instead of TIFF's
+#jpegBool = arcpy.GetParameter(7)
 
 #Create Scratch Directory
 scratchDir = outputDir + "\\" + "scratch"
 cropTemp = scratchDir + "\\crop"
-
 if not os.path.exists(scratchDir):
     os.mkdir(scratchDir)
 if not os.path.exists(cropTemp):
 	os.mkdir(cropTemp)
 
-#Set Data Frame Coordinate system to NAD 1927:
+#Set Data Frame Coordinate system:
 arcpy.AddMessage("Changing data frame coordinate system")
 secondary_functions.setDataFrameGCS(coordSys)
                          
@@ -64,6 +63,20 @@ secondary_functions.setDataFrameGCS(coordSys)
 dirList = os.listdir(tifDir)
 
 #Get list of tiffs, ensure it is just a list of tiffs with nothing else.  
+#NOTE: FOR UPGRADE TO ALLOW FOR JPG'S, THIS IS ONE PLACE YOU MUST CHANGE
+"""
+tifList = []
+if jpegBool:
+    for l in dirList:
+        if l[-4:] == ".jpg" or l[-4:] == ".JPG":
+            tiflist.append(l)
+else:
+    for l in dirList:
+        if l[-4:] == ".tif" or l[-4:] == ".TIF":
+            tifList.append(l)
+del dirList
+"""
+
 tifList = []
 for l in dirList:
     if l[-4:] == ".tif" or l[-4:] == ".TIF":
@@ -72,10 +85,9 @@ del dirList
 
 
 
-#Create errorlog, successlog.
+#Create errorlog
 arcpy.AddMessage("Creating error log - no problems yet, though")
 errorLog = error_handling.createErrorLog(outputDir)
-#successLog = createLog(outputDir)
 
 #Determine if the input zip file spatial index exists. Unzip it and use it.
 if os.path.exists(unionShapeFileZip):
@@ -93,7 +105,7 @@ if os.path.exists(unionShapeFileZip):
     secondary_functions.emptyTempFolder(tempZipDir)
     os.removedirs(tempZipDir)
 
-#Verify if the union shapefile exists.  Project, or define projection.
+#Otherwise, create a new one:
 if arcpy.Exists(unionShapefile) != True:
     arcpy.AddMessage("Creating new footprint file")
     #Create new footprint shapefile
@@ -102,7 +114,7 @@ if arcpy.Exists(unionShapefile) != True:
 
     
 
-#open workbook
+#Open workbook
 arcpy.AddMessage("Footprint file set up. Opening excel file index")
 workbook = xlrd.open_workbook(workbookName)
 
@@ -136,6 +148,7 @@ for tabIndex in worksheetList:
                                                 errorMessage, errorLog)
                 else:
                     arcpy.AddMessage(worksheetRow[0].value[:-6] + " seems to be alright. georeferencing.")
+                    #JPEG UPGRADE - This is location 3 where the code needs to be modified
                     georef.georef(worksheetRow, tifDir, 
                                   worksheetRow[0].value[:-5] + "C.tif",
                                   cropTemp, coordSys)
@@ -163,34 +176,27 @@ for tabIndex in worksheetList:
                                          "'s restored version is now being archived.")
                         
                         secondary_functions.emptyTempFolder(cropTemp)
+                        #JPEG UPGRADE - This is location 3 where the code needs to be modified
                         shutil.copyfile(whiteDir + "\\" + worksheetRow[0].value[:-5] + "W.tif" ,
                                                       cropTemp + "\\" + worksheetRow[0].value[:-5] + "W.tif")
                         zipArch = outputDir + "\\" + worksheetRow[0].value[:-5] + "White.zip"
                         secondary_functions.zipFolder(cropTemp, zipArch, metadataLoc)                     
                 secondary_functions.emptyTempFolder(cropTemp)
-                arcpy.AddMessage("All work with " + worksheetRow[0].value[:-6] + "is now done.")
+                arcpy.AddMessage("All work with " + worksheetRow[0].value[:-6] + " is now done.")
 
 os.removedirs(cropTemp)
-arcpy.AddMessage("All Geoprocessing done. Now archiving the spatial index feature class")
-arcpy.DeleteIdentical_management(unionShapefile ,
-                                 'filename;title;subtitle;province;year;Grid_Type;Scale', '#', '0')
-
-
-
+arcpy.AddMessage("All Geoprocessing done. Now removing duplicate features in the feature class")
 #Remove duplicate features
 fields = ["filename","Grid_Type"]
-arcpy.AddMessage("Removing identical features in spatial index...")
 arcpy.DeleteIdentical_management(unionShapefile, fields)
 #Final Cleanup
-arcpy.AddMessage("Indexing complete. Carrying out final stages")
+arcpy.AddMessage("Duplicates removed.  Now adding a time field to the feature class.")
 if arcpy.Exists(scratchDir + "\\" + "raster_footprint.shp"):
     secondary_functions.removeLayers()
     arcpy.AddField_management(scratchDir + "\\" + "raster_footprint.shp", "DATE_YEAR", "DATE")
     arcpy.ConvertTimeField_management(scratchDir + "\\" + "raster_footprint.shp","year","yyyy",
                                       "DATE_YEAR")
-    #arcpy.AddMessage("Converting Feature Class into Layer")
-    #arcpy.MakeFeatureLayer_management(scratchDir + "\\" + "raster_footprint.shp",
-    #                                  outputDir + "\\" +  "spatial_index_lyr")
+    arcpy.AddMessage("Now zipping the shapefile")
     secondary_functions.zipFolder(scratchDir, outputDir + "\\" + "FOOTPRINT_SHP" + ".zip",metadataLoc)
     secondary_functions.emptyTempFolder(scratchDir)
     os.removedirs(scratchDir)
