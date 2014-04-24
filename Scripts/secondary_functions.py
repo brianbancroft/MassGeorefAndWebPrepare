09c0bd36db3ff5450952b1e524b708f0e016a837
Brian Bancroft <bancroft.bw@gmail.com>
2014-04-24 11:04:48 -0400

    Removed "Error Log" and "Success Log" Functions
    Error log is a function that exists in the index_error_handling.py file, where as the Create error log function here has not been tested. 
    
    Success log was not used, and has been removed.

--- Scripts/secondary_functions.py
+++ Scripts/secondary_functions.py
@@ -12,59 +12,6 @@ def setDataFrameGCS(c):
     df.spatialReference = arcpy.SpatialReference(c)
     del mxd
 
-###Function:  Create Blank Error Log
-def createErrorLog(dir):
-    #construct sheet
-    fc = "errorLog-" + str(datetime.date.today()) + ".xls"
-    wb = xlwt.Workbook(encoding = 'UTF-8')
-    ws = wb.add_sheet("log")
-
-    #Populate first row with headings
-    cellEntry = ["File","Map_Title","Map_Subtitle","grid_type","scale",
-                 "province_state","bounding","SE_LAT","SE_LONG","SW_LAT",
-                 "SW_LONG","NW_LAT","NW_LONG","NE_LAT","NE_LONG",
-                 "YEAR","ERROR"]
-    n = 0
-    while n < len(cellEntry):
-        ws.write(0,n,cellEntry[n])
-        n += 1
-
-    #save book
-    if os.path.isfile("C:/Temp" + "/" + fc) == False:
-        wb.save(dir + "/" + fc)
-    else:
-        fc  = ("errorLog-" +
-               datetime.datetime.now().strftime("%m-%d_t%H%M") + ".xls")
-        wb.save(dir + "/" + fc)
-
-    return(dir + "/" + fc)
-
-###Function: Create Success Log
-def createLog(dir):
-    #construct sheet
-    fc = "success_log-" + str(datetime.date.today()) + ".xls"
-    wb = xlwt.Workbook(encoding = 'UTF-8')
-    ws = wb.add_sheet("log")
-
-    #Populate first row
-    cellEntry = ["File","Map_Title","Map_Subtitle","grid_type","scale",
-                 "province_state","bounding","SE_LAT","SE_LONG","SW_LAT",
-                 "SW_LONG","NW_LAT","NW_LONG","NE_LAT","NE_LONG",
-                 "YEAR"]
-    n = 0
-    while n < len(cellEntry):
-        ws.write(0,n,cellEntry[n])
-        n += 1
-
-    #save book
-    if os.path.isfile("C:/Temp" + "/" + fc) == False:
-        wb.save(dir + "/" + fc)
-    else:
-        fc  = ("success_log-" +
-               datetime.datetime.now().strftime("%m-%d_t%H%M") + ".xls")
-        wb.save(dir + "/" + fc)
-    return(dir + "/" + fc)
-
 ###Function: Remove Layers in data frame
 def removeLayers():
     mxd = arcpy.mapping.MapDocument("CURRENT")
